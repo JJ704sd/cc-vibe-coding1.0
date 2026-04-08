c163266da39d85ece14489f84ac11b1b4f20f02a
JJ704SD <2756695045@qq.com>
2026-04-08 19:05:16 +0800

    feat: add map projection overlay shell

--- apps/web/src/components/map/index.ts
+++ apps/web/src/components/map/index.ts
@@ -1,4 +1,5 @@
 export { MapBase3DView } from '@/components/map/MapBase3DView';
 export { MapRelationshipPanel } from '@/components/map/MapRelationshipPanel';
 export { MediaClusterLayer } from '@/components/map/MediaClusterLayer';
+export { MapProjectionOverlay } from '@/components/map/MapProjectionOverlay';
 export { StarRelationshipLayer } from '@/components/map/StarRelationshipLayer';
